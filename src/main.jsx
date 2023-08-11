7223fd07025f62f6f63093084003717a44014cc3
Andres Huanca Namuche <andreshuanca@upeu.edu.pe>
2023-08-10 21:07:00 -0500

    config para github03

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-// import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
-
+import { BrowserRouter } from 'react-router-dom';
 import { JournalApp } from './JournalApp.jsx';
 import { store } from './store';
 import './styles.css';
@@ -11,13 +9,11 @@ import './styles.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>  
     <Provider store={ store } > {/* Redux */}
-      {/* <BrowserRouter > Rutas  */}
-      <HashRouter >
+      <BrowserRouter > {/* Rutas  */}
+
         <JournalApp /> {/* App */}
 
-      </HashRouter>
-
-      {/* </BrowserRouter> */}
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
